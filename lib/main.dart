696ebd3a7f3af6e3fa39ba768579677bbc182a3d
unknown <annisa22@gmail.com>
2022-09-18 23:57:38 +0700

    commit prak mobile

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home : Scaffold(
+        appBar: AppBar(
+          title : Text('Login Screen'),
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          //child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:<Widget> [
+             Container(
+               alignment: Alignment.center,
+               padding: const EdgeInsets.fromLTRB(10, 10, 10, 50),
+               child :FlutterLogo(
+                 size: 35,
+               )
+             ),
+
+             Container(
+               padding: const EdgeInsets.all(15),
+               child: TextField(
+                 decoration: InputDecoration(
+                   border: OutlineInputBorder(
+                     borderRadius: BorderRadius.circular(85.0),
+                   ),
+                   labelText: 'Username',
+                 ),
+               ),
+             ),
+             Container(
+               padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+               child: TextField(
+                 obscureText : true,
+
+                 decoration: InputDecoration(
+                   border: OutlineInputBorder(
+                     borderRadius: BorderRadius.circular(85.0),
+                   ),
+                   labelText: 'Password',
+                 ),
+               ),
+             ),
+
+             Container(
+               height: 75,
+               padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+               child: ElevatedButton(
+                 style: ElevatedButton.styleFrom(
+                   minimumSize: const Size.fromHeight(50),
+                   primary: Colors.amber,
+                 ),
+                 child: const Text('Log In'),
+                 onPressed: () {
+                 },
+               )
+             ),
+
+                 TextButton(
+                   onPressed: (){},
+                   child: Text(
+                     'Forgot Password?',
+                         style: TextStyle(
+                           color: Colors.amber,
+                           ),
+                      ),
+                    ),
+                    ],
+                  ),
+                ),
+            ),
+         // ),
+    );
+  }
+}
